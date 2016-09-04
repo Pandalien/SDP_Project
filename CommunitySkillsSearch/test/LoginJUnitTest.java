ff8fc9cab888702781e7d63400e6fc09a1b7fa4b
yaton1119 <matt@Matts-MacBook-Air.local>
2016-09-04 17:07:49 +1200

    Complete Login unit test

--- CommunitySkillsSearch/test/LoginJUnitTest.java
+++ CommunitySkillsSearch/test/LoginJUnitTest.java
@@ -10,12 +10,14 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import servlets.Login;
 
 /**
  *
  * @author matt
  */
 public class LoginJUnitTest {
+    Login login;
     
     public LoginJUnitTest() {
     }
@@ -30,15 +32,68 @@ public class LoginJUnitTest {
     
     @Before
     public void setUp() {
+        this.login = new Login();
     }
     
+    @Test
+    public void testLoginEmptyEmail() {
+        assertFalse(login.validateEmail(""));                       // empty email
+    }
+    
+    @Test
+    public void testLoginInvalidEmail1() {
+        assertFalse(login.validateEmail("usernamedomain.com"));     // missing @ symbol
+    }
+    
+    @Test
+    public void testLoginInvalidEmail2() {
+        assertFalse(login.validateEmail("user@name@domain.com"));   // more than one @ symbol
+    }
+    
+    @Test
+    public void testLoginInvalidEmail3() {
+        assertFalse(login.validateEmail("user name@domain.com"));   // whitespace in username
+    }
+    
+    @Test
+    public void testLoginInvalidEmail4() {
+        assertFalse(login.validateEmail("username@domain..com"));   // consecutive dots in domain
+    }
+    
+    @Test
+    public void testLoginInvalidEmail5() {
+        assertFalse(login.validateEmail(".username@domain.com"));   // username starts with dot
+    }
+    
+    @Test
+    public void testLoginEmptyPassword() {
+        assertFalse(login.validatePassword(""));                    // empty password
+    }
+    
+    @Test
+    public void testLoginInvalidPassword1() {
+        assertFalse(login.validatePassword("Pw1"));                 // less than 4 characters
+    }
+    
+    @Test
+    public void testLoginInvalidPassword2() {
+        assertFalse(login.validatePassword("Password 1"));          // whitespace in password
+    }
+
+    @Test
+    public void testLoginInvalidPassword3() {
+        assertFalse(login.validatePassword("PASSWORD1"));            // at least one lowercase letter
+    }
+    @Test
+    public void testLoginInvalidPassword4() {
+        assertFalse(login.validatePassword("password1"));            // at least one uppercase letter
+    }
+    @Test
+    public void testLoginInvalidPassword5() {
+        assertFalse(login.validatePassword("12345678"));            // at least one uppercase and lowercase letter
+    }    
+
     @After
     public void tearDown() {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
